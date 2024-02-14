61c58c44c7c622bb9ff336b3e4dce9de89d60adf
Harris Butler <harris.butler@auctane.com>
2024-02-14 17:41:59 -0600

    clean up

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,13 @@
-export { auth as middleware } from "auth"
+export { auth as middleware } from "auth";
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+};
