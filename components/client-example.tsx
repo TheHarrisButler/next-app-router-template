2544b0b19eb8d9cb02ebe6836a87b8876a51ff09
Harris Butler <harris.butler@auctane.com>
2024-02-14 18:05:11 -0600

    some more clean up

--- components/client-example.tsx
+++ components/client-example.tsx
@@ -1,45 +1,8 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { useState } from "react";
 import SessionData from "./session-data";
 import CustomLink from "./custom-link";
-
-const UpdateForm = () => {
-  const { data: session, update } = useSession();
-  const [name, setName] = useState(session?.user?.name ?? "");
-
-  if (!session?.user) return null;
-  return (
-    <>
-      <h2 className="text-xl font-bold">Updating the session</h2>
-      <form
-        onSubmit={async () => {
-          if (session) {
-            const newSession = await update({
-              ...session,
-              user: { ...session.user, name },
-            });
-            console.log({ newSession });
-          }
-        }}
-        className="flex items-center w-full max-w-sm space-x-2"
-      >
-        <Input
-          type="text"
-          placeholder={session.user.name ?? ""}
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <Button type="submit">Update</Button>
-      </form>
-    </>
-  );
-};
 
 export default function ClientExample() {
   const { data: session, status } = useSession();
@@ -58,8 +21,7 @@ export default function ClientExample() {
         <CustomLink href="https://react.devreference/nextjs/react/use-client">
           <code>'use client'</code>
         </CustomLink>{" "}
-        directive at the top of the file to enable client side rendering, and
-        the{" "}
+        directive at the top of the file to enable client side rendering, and the{" "}
         <CustomLink href="https://nextjs.authjs.dev/react#sessionprovider">
           <code>SessionProvider</code>
         </CustomLink>{" "}
@@ -70,12 +32,7 @@ export default function ClientExample() {
         to provide the session data.
       </p>
 
-      {status === "loading" ? (
-        <div>Loading...</div>
-      ) : (
-        <SessionData session={session} />
-      )}
-      <UpdateForm />
+      {status === "loading" ? <div>Loading...</div> : <SessionData session={session} />}
     </div>
   );
 }
