bc2551d0aab20a62ac6c4ee4b707eaad09a7929b
Harris Butler <harrishbutler1@gmail.com>
2024-02-19 20:17:35 -0600

    icons

---
+++ components/icons/done-icon.tsx
@@ -0,0 +1,7 @@
+type Props = React.SVGProps<SVGSVGElement>;
+
+export const DoneIcon = ({ ...props }: Props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" {...props}>
+    <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+  </svg>
+);
