1de07e22af8dd7be3cd9b9342fd61ff99cee9679
Harris Butler <harrishbutler1@gmail.com>
2024-02-19 20:18:18 -0600

    clean up

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import CustomLink from "./custom-link";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col w-full px-4 mx-0 my-4 space-y-1 text-sm md:max-w-3xl md:my-12 md:mx-auto sm:px-6 md:h-5 md:items-center md:space-y-0 md:space-x-4 md:flex-row">
+    <footer className="flex flex-col w-full px-4 mx-0 my-4 space-y-1 text-sm md:max-w-7xl md:my-12 md:mx-auto sm:px-6 md:h-5 md:items-center md:space-y-0 md:space-x-4 md:flex-row">
       <CustomLink href="/policy">Policy</CustomLink>
     </footer>
   );
